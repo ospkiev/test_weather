9d1ccc9b8407fd8aa0691b4cd9f7e4e24a69c4dd
Pavel Karpenko <ospkiev@gmail.com>
2019-03-22 17:54:52 +0200

    Updates

---
+++ precache-manifest.9041eb23c080dacc132c2905d3157abb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c50c5d4341b0c0695fa",
+    "url": "/test_weather/static/js/runtime~main.3c50c5d4.js"
+  },
+  {
+    "revision": "29df52ae024e2a2b8ac6",
+    "url": "/test_weather/static/js/main.29df52ae.chunk.js"
+  },
+  {
+    "revision": "6500628cb0f8c403bfb2",
+    "url": "/test_weather/static/js/1.6500628c.chunk.js"
+  },
+  {
+    "revision": "29df52ae024e2a2b8ac6",
+    "url": "/test_weather/static/css/main.2c5e4086.chunk.css"
+  },
+  {
+    "revision": "6500628cb0f8c403bfb2",
+    "url": "/test_weather/static/css/1.78968fc4.chunk.css"
+  },
+  {
+    "revision": "483f50fb31433c5b8b7ad87b85943591",
+    "url": "/test_weather/index.html"
+  }
+];
